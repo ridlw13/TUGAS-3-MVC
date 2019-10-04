21b0f783004416d15227bd116d54da29c721803c
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-10-04 11:19:09 +0700

    Membuat template header dan footer untuk setiap view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -6,13 +6,20 @@ class About extends Controller {
         $data['nama'] = $nama;
         $data['pekerjaan'] = $pekerjaan;
         $data['umur'] = $umur;
-
-        $this->view('about/index', $data);
+        $data['judul'] = 'About' ;
+        $this->view('template/header', $data);
+        $this->view('about/index' ,$data);
+        $this->view('template/footer');
+        
     
     }
     public function page(){
-    
+        
+        $data['judul'] = 'Pages';
+        $this->view('template/header', $data);
         $this->view('about/page');
+        $this->view('template/footer');
+        
     }
 }
 
--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,7 +3,11 @@
 class Home extends Controller {
     public function index(){
         
+        $data['judul'] = Home;
+        $this->view('template/header', $data);
         $this->view('home/index');
+        $this->view('template/footer');
+        
     }
 }
 
