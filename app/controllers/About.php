78791d256776ea082817bfe40b267cb32844dc26
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-10-04 10:53:24 +0700

    Membuat view untuk halaman About dan page

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,13 @@
 <?php
 
-class About {
+class About extends Controller {
     public function index($nama = 'Ridlwan', $pekerjaan = 'Guru' , $umur = '20'){
-        echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan,
-        umur saya adalah $umur tahun.";
+        $this->view('about/index');
+    
     }
     public function page(){
-        echo 'About/page';
+    
+        $this->view('about/page');
     }
 }
 
