30a0ed74858f91bb26b68d4746acdaa6ed63866b
ridlw13 <ridlwanbaks@mail.unpas.ac.id>
2019-10-05 13:50:19 +0700

    Membuat halaman detail mahasiswa

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -14,4 +14,10 @@ class Mahasiswa_model {
         return $this->db->resultSet();
 
     }
+
+    public function getMahasiswaById($id){
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
 }
